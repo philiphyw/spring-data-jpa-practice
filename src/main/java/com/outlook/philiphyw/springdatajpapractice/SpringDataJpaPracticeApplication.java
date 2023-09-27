93ca79f6e6748ed593002cfff2ec0952e228ff6e
Philip HYW <75644553+philiphyw@users.noreply.github.com>
2023-09-27 13:51:24 -0400

    initial commit from local

---
+++ src/main/java/com/outlook/philiphyw/springdatajpapractice/SpringDataJpaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.outlook.philiphyw.springdatajpapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outlook/philiphyw/springdatajpapractice/SpringDataJpaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outlook.philiphyw.springdatajpapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
